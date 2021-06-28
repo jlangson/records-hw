97fe64b7ae11c4d0484b4cd08e855f64fa9f445b
Jared Langson <langson.jared@gmail.com>
2021-06-28 18:56:09 -0400

    OUtline remaining method declarations in RecordService

--- src/main/java/com/jared/records/service/RecordService.java
+++ src/main/java/com/jared/records/service/RecordService.java
@@ -17,16 +17,37 @@ public class RecordService {
     }
 
     //merge records
+    public static List<Record> mergeRecordLists(ArrayList<Record> listOfRecords){
+
+    }
 
     //sort by gender
+    public static List<Record> sortByGender(ArrayList<Record> records){
+
+    }
 
     //sort by birth date, ascending
+    public static List<Record> sortByBirthDateAscending(ArrayList<Record> records){
+
+    }
 
     //sort by last name, descending
+    public static List<Record> sortByLastNameDescending(ArrayList<Record> records){
+
+    }
 
     //output sorted by gender
+    public static void printGenderSort(){
+
+    }
 
     //output sorted by birth date, ascending
+    public static void printBirthDateAscendingSort(){
+
+    }
 
     //output sorted by last name descending
+    public static void printLastNameDescendingSort(){
+        
+    }
 }
