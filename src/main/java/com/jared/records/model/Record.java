2cb8ca939fcf093d62775715798c305bbe9ecd37
Jared Langson <langson.jared@gmail.com>
2021-06-28 17:55:08 -0400

    Create boilerplate for Record

--- src/main/java/com/jared/records/model/Record.java
+++ src/main/java/com/jared/records/model/Record.java
@@ -1,4 +1,70 @@
 package com.jared.records.model;
 
 public class Record {
+    private String lastName;
+    private String firstName;
+    private String gender;
+    private String favoriteColor;
+    private String dateOfBirth;
+
+    public Record(String lastName, String firstName, String gender, String favoriteColor, String dateOfBirth) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.gender = gender;
+        this.favoriteColor = favoriteColor;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getFavoriteColor() {
+        return favoriteColor;
+    }
+
+    public void setFavoriteColor(String favoriteColor) {
+        this.favoriteColor = favoriteColor;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Record{" +
+                "lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", favoriteColor='" + favoriteColor + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                '}';
+    }
+
+
 }
