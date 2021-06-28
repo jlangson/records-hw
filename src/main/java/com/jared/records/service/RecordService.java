f6e70bd674b762c7c94f4bfde23301fe4bb43480
Jared Langson <langson.jared@gmail.com>
2021-06-28 18:43:16 -0400

    Write comments on which RecordServices to build

--- src/main/java/com/jared/records/service/RecordService.java
+++ src/main/java/com/jared/records/service/RecordService.java
@@ -2,4 +2,21 @@ package com.jared.records.service;
 import com.jared.records.model.Record;
 
 public class RecordService {
+    //turn record from file into a Record
+
+    //turn all inputs into Record collection
+
+    //merge records
+
+    //sort by gender
+
+    //sort by birth date, ascending
+
+    //sort by last name, descending
+
+    //output sorted by gender
+
+    //output sorted by birth date, ascending
+
+    //output sorted by last name descending
 }
