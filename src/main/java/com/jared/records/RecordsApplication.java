c4432cb26dd7fa97e52e2fd1c3fbdf58916697ee
Jared Langson <langson.jared@gmail.com>
2021-06-28 18:10:40 -0400

    Read pipes.csv in main

--- src/main/java/com/jared/records/RecordsApplication.java
+++ src/main/java/com/jared/records/RecordsApplication.java
@@ -3,11 +3,18 @@ package com.jared.records;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @SpringBootApplication
 public class RecordsApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(RecordsApplication.class, args);
+
+		String pipes = new String(Files.readAllBytes(Paths.get("src/main/resources/data/pipes.csv")));
+		System.out.println(pipes);
 	}
 
 }
