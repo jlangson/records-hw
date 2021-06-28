9e9df836799985b90a483f433e166a29aaa6cbad
Jared Langson <langson.jared@gmail.com>
2021-06-28 19:13:02 -0400

    Create RecordServiceTest.java

---
+++ src/test/java/com/jared/records/RecordServiceTest.java
@@ -0,0 +1,8 @@
+package com.jared.records;
+import com.jared.records.model.Record;
+import com.jared.records.service.RecordService;
+
+import org.junit.jupiter.api.Test;
+
+public class RecordServiceTest {
+}
