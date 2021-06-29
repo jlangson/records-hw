f5dce7c4d94991461bb24de6b17e6c5c10da3677
Jared Langson <langson.jared@gmail.com>
2021-06-29 16:05:19 -0400

    Reads files and merges into one collection of Records

--- src/main/java/com/jared/records/RecordsConsole.java
+++ src/main/java/com/jared/records/RecordsConsole.java
@@ -2,10 +2,26 @@ package com.jared.records;
 import com.jared.records.model.Record;
 import com.jared.records.service.RecordService;
 
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 //for running part one of the assignment
 public class RecordsConsole {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        //to store the inputs from every file before they are merged
+        ArrayList<ArrayList<Record>> recordOfRecords = new ArrayList<>();
+        ArrayList<Record> mainList = new ArrayList<>(); //where merged records are stored
 
+        for(int i=0; i < RecordService.paths.length; i++){
+            ArrayList<Record> records = RecordService.createAllRecordsFromFile(RecordService.paths[i]);
+            recordOfRecords.add(records);
+        }
+
+        for(int i=0; i < recordOfRecords.size(); i++){
+            mainList.addAll(recordOfRecords.get(i));
+        }
+        System.out.println(mainList.size()); //expect 300
     }
 }
