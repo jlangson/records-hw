310aa7f850a474da26c7f46d16ca5023a35c9ee7
Jared Langson <langson.jared@gmail.com>
2021-06-28 21:58:12 -0400

    Create RecordTest

---
+++ src/test/java/com/jared/records/RecordTest.java
@@ -0,0 +1,11 @@
+package com.jared.records;
+import org.junit.jupiter.api.Test;
+import com.jared.records.model.Record;
+
+public class RecordTest {
+
+    @Test
+    public void areRecordsWithSameValuesEqual(){
+        
+    }
+}
