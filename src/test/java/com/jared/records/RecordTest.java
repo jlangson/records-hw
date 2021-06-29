58230caace9542dc0ed1bbedebfb2b0beee2fe9a
Jared Langson <langson.jared@gmail.com>
2021-06-29 18:03:27 -0400

    Refactor RecordTest to use BeforeAll

--- src/test/java/com/jared/records/RecordTest.java
+++ src/test/java/com/jared/records/RecordTest.java
@@ -1,14 +1,20 @@
 package com.jared.records;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import com.jared.records.model.Record;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RecordTest {
+    static Record recordOne;
+    static Record recordTwo;
 
-    public static final Record recordOne = new Record("Langson", "Jared", "male", "blue", "10/14/1988");
-    public static final Record recordTwo = new Record("Langson", "Jared", "male", "blue", "10/14/1988");
-
+    @BeforeAll
+    static void setup(){
+        recordOne = new Record("Langson", "Jared", "male", "blue", "10/14/1988");
+        recordTwo = new Record("Langson", "Jared", "male", "blue", "10/14/1988");
+    }
+    
     @Test
     public void areRecordsWithSameValuesEqual(){
         assertEquals(recordOne, recordTwo);
