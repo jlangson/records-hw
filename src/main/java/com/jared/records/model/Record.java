50ef40a3bc645fb26d1b285a4bd7e25b0b8d5dae
Jared Langson <langson.jared@gmail.com>
2021-06-29 15:50:45 -0400

    Create outputs for different String orderings of Record fields

--- src/main/java/com/jared/records/model/Record.java
+++ src/main/java/com/jared/records/model/Record.java
@@ -72,6 +72,7 @@ public class Record {
         return month + "/"  + day + "/" + year;
     }
 
+
     @Override
     public String toString() {
         return "Record{" +
@@ -104,5 +105,39 @@ public class Record {
         return Objects.hash(lastName, firstName, gender, favoriteColor, dateOfBirth);
     }
 
+    //for displaying results when sorted by gender so gender appears first.
+    public String outputByGenderFirstThenLastName(){
+        return "Record{" +
+                ", gender='" + gender + '\'' +
+                "lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", favoriteColor='" + favoriteColor + '\'' +
+                ", dateOfBirth='" + formatDateOfBirth() + '\'' +
+                '}';
+    }
+
+    //for displaying results when sorted by birthday so birthday appears first
+    public String outputByBirthDateFirst(){
+        return "Record{" +
+                ", dateOfBirth='" + formatDateOfBirth() + '\'' +
+                "lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", favoriteColor='" + favoriteColor + '\'' +
+                '}';
+    }
+
+    //for displaying by LastName first
+    //currently identical to toString(). Added in case toString changes so there is a consistent access to getting
+    //output with lastName as the first column.
+    public String outputByLastNameFirst(){
+        return "Record{" +
+                "lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", favoriteColor='" + favoriteColor + '\'' +
+                ", dateOfBirth='" + formatDateOfBirth() + '\'' +
+                '}';
+    }
 
 }
