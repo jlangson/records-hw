04b578d4acceed8c767af6b69ece495ec2fa3b31
Jared Langson <langson.jared@gmail.com>
2021-06-29 15:52:55 -0400

    Create main to run part1 to avoid needing to start Spring for part1

---
+++ src/main/java/com/jared/records/RecordsConsole.java
@@ -0,0 +1,11 @@
+package com.jared.records;
+import com.jared.records.model.Record;
+import com.jared.records.service.RecordService;
+
+//for running part one of the assignment
+public class RecordsConsole {
+
+    public static void main(String[] args) {
+
+    }
+}
