83624f43aa5ba1bbccb10447d5c187f9f024a651
Jared Langson <langson.jared@gmail.com>
2021-06-28 22:08:03 -0400

    Write RecordTest for deep equality

--- src/test/java/com/jared/records/RecordTest.java
+++ src/test/java/com/jared/records/RecordTest.java
@@ -2,10 +2,14 @@ package com.jared.records;
 import org.junit.jupiter.api.Test;
 import com.jared.records.model.Record;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RecordTest {
 
     @Test
     public void areRecordsWithSameValuesEqual(){
-        
+        Record recordOne = new Record("Langson", "Jared", "male", "blue", "10/14/1988");
+        Record recordTwo = new Record("Langson", "Jared", "male", "blue", "10/14/1988");
+        assertEquals(recordOne, recordTwo);
     }
 }
