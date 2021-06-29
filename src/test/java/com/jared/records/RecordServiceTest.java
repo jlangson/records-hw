8e07e3cc0f8d16b04e82b65fcc4079d51e7bb4df
Jared Langson <langson.jared@gmail.com>
2021-06-28 20:03:37 -0400

    Refactor by delete createDummyRows and move rows to global scope

--- src/test/java/com/jared/records/RecordServiceTest.java
+++ src/test/java/com/jared/records/RecordServiceTest.java
@@ -8,31 +8,25 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class RecordServiceTest {
-
     //creates rows to represent data from a read file
     //files can be pipe, comma, or space separated
-    @BeforeAll
-    public static void createDummyRows(){
-        String[] commaRows = new String[]{
-                "Stacy, Jacobson, female, yellow, 05/12/21",
-                "Cyrus, Cardenas, male, violet, 05/10/22",
-                "Zeph, Holloway, male, violet, 03/31/22"
-        };
-        String[] pipeRows = new String[]{
-                "Troy | Dillard | female | red | 04/14/22",
-                "Florence | Lawson | female | orange | 10/01/20",
-                "Athena | Ortiz | male | yellow | 12/14/20"
-        };
-        String[] spaceRows = new String[]{
-                "Kimberly Zimmerman female blue 04/23/22",
-                "Francis White male indigo 09/19/20",
-                "Austin Simon female violet 05/30/22"
-        };
-    }
-
+   public static final String[] commaRows = new String[]{
+            "Stacy, Jacobson, female, yellow, 05/12/21",
+            "Cyrus, Cardenas, male, violet, 05/10/22",
+            "Zeph, Holloway, male, violet, 03/31/22"};
+   public static final String[] pipeRows = new String[]{
+            "Troy | Dillard | female | red | 04/14/22",
+            "Florence | Lawson | female | orange | 10/01/20",
+            "Athena | Ortiz | male | yellow | 12/14/20"};
+    public static final String[] spaceRows = new String[]{
+            "Kimberly Zimmerman female blue 04/23/22",
+            "Francis White male indigo 09/19/20",
+            "Austin Simon female violet 05/30/22"};
+    
+    //following tests are for rowToRecord
     @Test
-    public void createRecordFromCommaRow(){
-
+    public static void createRecordFromCommaRow(){
+        Record record = RecordService.rowToRecord(commaRows[0]);
     }
 
     @Test
