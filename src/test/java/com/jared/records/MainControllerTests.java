ab05af5a0f011d1e87187d4fb134e339418c80a0
Jared Langson <langson.jared@gmail.com>
2021-06-30 12:08:12 -0400

    Wrote outline of MainControllerTests

---
+++ src/test/java/com/jared/records/MainControllerTests.java
@@ -0,0 +1,64 @@
+package com.jared.records;
+import org.junit.jupiter.api.Test;
+
+public class MainControllerTests {
+
+    //three tests per GET endpoint: response code, headers, and payload
+    //POST needs two response code tests since it can return success or fail
+
+    @Test
+    public void testGetRecordsByGender_ResponseCode(){
+
+    }
+    @Test
+    public void testGetRecordsByGender_Headers(){
+
+    }
+    @Test
+    //should return a json with the first column is gender
+    public void testGetRecordsByGender_Payload(){
+
+    }
+
+    @Test
+    public void testGetRecordsByBirthDate_ResponseCode(){
+
+    }
+    @Test
+    public void testGetRecordsByBirthDate_Headers(){
+
+    }
+    @Test
+    public void testGetRecordsByBirthDate_Payloiad(){
+
+    }
+
+    @Test
+    public void testGetRecordsByLastName_ResponseCode(){
+
+    }
+    @Test
+    public void testGetRecordsbyLastName_Headers(){
+
+    }
+    @Test
+    public void testGetRecordsByLastName_Payload(){
+
+    }
+
+    @Test
+    public void testCreateRecordSuccess_ResponseCode(){
+
+    }
+    @Test
+    public void testCreateRecordFailure_ResponseCode(){
+
+    }
+    @Test
+    public void testCreateRecord_Header(){
+
+    }
+
+
+
+}
