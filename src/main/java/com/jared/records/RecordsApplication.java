2f518d5ffb4e64709ce661d7d92850f180f1d086
Jared Langson <langson.jared@gmail.com>
2021-06-30 11:45:17 -0400

    Create field in RecordsApplication to gather starting data that can be passed to the controller for setting up the repository

--- src/main/java/com/jared/records/RecordsApplication.java
+++ src/main/java/com/jared/records/RecordsApplication.java
@@ -1,16 +1,21 @@
 package com.jared.records;
 
+import com.jared.records.model.Record;
+import com.jared.records.service.RecordService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class RecordsApplication {
+	public static ArrayList<Record> initialData; //to pass the data to the controller.
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		initialData = RecordsConsole.readData(RecordService.paths);
 		SpringApplication.run(RecordsApplication.class, args);
 	}
 
