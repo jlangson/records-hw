1d6648a01d0898b6748dea145556ca97ac03e28d
Jared Langson <langson.jared@gmail.com>
2021-06-30 10:58:48 -0400

    Create a fake repository to abstract 'database' calls for the controller

---
+++ src/main/java/com/jared/records/respository/RecordRepositoryFake.java
@@ -0,0 +1,12 @@
+package com.jared.records.respository;
+
+import com.jared.records.model.Record;
+
+import java.util.ArrayList;
+
+//simulates a DB
+public class RecordRepositoryFake {
+    private ArrayList <Record> genderSorted;
+    private ArrayList <Record> birthDateSorted;
+    private ArrayList <Record> lastNameSorted;
+}
