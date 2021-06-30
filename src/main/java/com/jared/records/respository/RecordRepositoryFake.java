186f784f1b3245198b9835dc027636e5b8258bb9
Jared Langson <langson.jared@gmail.com>
2021-06-30 11:01:19 -0400

    Create constructor for RecordRepositoryFake

--- src/main/java/com/jared/records/respository/RecordRepositoryFake.java
+++ src/main/java/com/jared/records/respository/RecordRepositoryFake.java
@@ -1,6 +1,7 @@
 package com.jared.records.respository;
 
 import com.jared.records.model.Record;
+import com.jared.records.service.RecordService;
 
 import java.util.ArrayList;
 
@@ -9,4 +10,11 @@ public class RecordRepositoryFake {
     private ArrayList <Record> genderSorted;
     private ArrayList <Record> birthDateSorted;
     private ArrayList <Record> lastNameSorted;
+
+    //takes an AL of records and then creates the three different sorting criteria.
+    RecordRepositoryFake(ArrayList<Record> records) {
+        genderSorted = RecordService.sortByGenderThenLastNameAscending(records);
+        birthDateSorted = RecordService.sortByBirthDateAscending(records);
+        lastNameSorted = RecordService.sortByLastNameDescending(records);
+    }
 }
