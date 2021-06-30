a0e3a2514e564d487b0152d3626b3eb4fa7f4e03
Jared Langson <langson.jared@gmail.com>
2021-06-30 12:56:46 -0400

    Write test for ResponseCode of GetRecordsByGender

--- src/test/java/com/jared/records/MainControllerTests.java
+++ src/test/java/com/jared/records/MainControllerTests.java
@@ -1,13 +1,48 @@
 package com.jared.records;
+import com.jared.records.controller.MainController;
+import com.jared.records.model.Record;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.methods.HttpUriRequest;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.core5.http.HttpResponse;
+import org.apache.hc.core5.http.HttpStatus;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainControllerTests {
+//    public static ResponseEntity<ArrayList<Record>> genderResponse;
+//    public static ResponseEntity<ArrayList<Record>> birthDateResponse;
+//    public static ResponseEntity<ArrayList<Record>> lastNameResponse;
+//    public static ResponseEntity<Void> goodCreateResponse;
+//    public static ResponseEntity<Void> badCreateResponse;
+//
+//    //three tests per GET endpoint: response code, headers, and payload
+//    //POST needs two response code tests since it can return success or fail
+//
+    @BeforeAll
+    public static void setup() throws IOException{
+        String[] args = new String[]{""};
+        RecordsApplication.main(args);
 
-    //three tests per GET endpoint: response code, headers, and payload
-    //POST needs two response code tests since it can return success or fail
+    }
+
+    @AfterAll
+    public static void cleanup(){
+        System.exit(0);
+    }
 
     @Test
-    public void testGetRecordsByGender_ResponseCode(){
+    public void testGetRecordsByGender_ResponseCode() throws IOException {
+        HttpUriRequest request = new HttpGet("http://localhost:8080/records/gender");
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+
+        assertEquals(HttpStatus.SC_OK, httpResponse);
 
     }
     @Test
