07e850860e315f7516a8277d367646f42f1bbfe7
Jared Langson <langson.jared@gmail.com>
2021-06-30 11:34:43 -0400

    Refactor RecordsConsole to create a method that handles the initial reading of the files. So this can be called for setting up the API pseudodatabase

--- src/main/java/com/jared/records/RecordsConsole.java
+++ src/main/java/com/jared/records/RecordsConsole.java
@@ -9,25 +9,32 @@ import java.util.ArrayList;
 //for running part one of the assignment
 public class RecordsConsole {
 
-    public static void main(String[] args) throws IOException {
+    public static ArrayList<Record> readData(String[] filePaths) throws IOException{
         //to store the inputs from every file before they are merged
         ArrayList<ArrayList<Record>> recordOfRecords = new ArrayList<>();
-        ArrayList<Record> mainList = new ArrayList<>(); //where merged records are stored
+        ArrayList<Record> initialList = new ArrayList<>(); //where merged records are stored
 
-        for(int i=0; i < RecordService.paths.length; i++){
-            ArrayList<Record> records = RecordService.createAllRecordsFromFile(RecordService.paths[i]);
+        for(int i=0; i < filePaths.length; i++){
+            ArrayList<Record> records = RecordService.createAllRecordsFromFile(filePaths[i]);
             recordOfRecords.add(records);
         }
 
         for(int i=0; i < recordOfRecords.size(); i++){
-            mainList.addAll(recordOfRecords.get(i));
+            initialList.addAll(recordOfRecords.get(i));
         }
-        System.out.println(mainList.size()); //expect 300
+        System.out.println(initialList.size()); //expect 300
+
+        return initialList;
+    }
+
+    public static void main(String[] args) throws IOException {
+        ArrayList<Record> initialList = readData(RecordService.paths);
+
 
         //keeps three sorted ArrayLists so program doesn't need to resort when a different output is called
-        ArrayList<Record> recordsSortedByGenderThenLastNameAscending = RecordService.sortByGenderThenLastNameAscending(mainList);
-        ArrayList<Record> recordsSortedByBirthDateAscending = RecordService.sortByBirthDateAscending(mainList);
-        ArrayList<Record> recordsSortedByLastNameDescending = RecordService.sortByLastNameDescending(mainList);
+        ArrayList<Record> recordsSortedByGenderThenLastNameAscending = RecordService.sortByGenderThenLastNameAscending(initialList);
+        ArrayList<Record> recordsSortedByBirthDateAscending = RecordService.sortByBirthDateAscending(initialList);
+        ArrayList<Record> recordsSortedByLastNameDescending = RecordService.sortByLastNameDescending(initialList);
 
         //ouputs
         System.out.println("Records sorted by gender then lastname in ascending order\n==========");
